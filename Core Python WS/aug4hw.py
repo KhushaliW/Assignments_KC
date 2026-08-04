df30cc7477bbbb504ec96a0cb9e6d002395436cb
Khushali Wagh <147276201+KhushaliW@users.noreply.github.com>
2026-08-04 22:59:23 +0530

    Add files via upload

---
+++ Core Python WS/aug4hw.py
@@ -0,0 +1,52 @@
+# Task 1: Identity Card
+name="Khushali Wagh"
+age=24
+city="Pune"
+dob="31/5/2002"
+collage="TKA"
+blood_group="A+"
+mobile_no="xxxxxxx185"
+roll_no="TKA1341"
+
+print()
+print("TASK 1: Identity Card")
+print()
+print("----- My ID Card -----")
+print("Name:",name)
+print("Age:",age)
+print("City:",city)
+print("DOB:",dob)
+print("Collage:",collage)
+print("Blood Group:",blood_group)
+print("Mobile No:",mobile_no)
+print("Roll No:",roll_no)
+print("-----------------------")
+print()
+
+# Task 2: Find data type
+a=25
+b=25.5
+c="Twenty Five"
+d=True
+e=2+3j
+
+print("TASK 2: Find data type")
+print()
+print("Data type of a:",type(a))
+print("Data type of b:",type(b))
+print("Data type of c:",type(c))
+print("Data type of d:",type(d))
+print("Data type of e:",type(e))
+print()
+
+# Task 3: Memory Dectective
+v1=100
+v2=200
+v3=100
+
+print("TASK 3: Memory Dectective")
+print()
+print("Memory address of v1:",id(v1))
+print("Memory address of v2:",id(v2))
+print("Memory address of v3:",id(v3))
+print()
